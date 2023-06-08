19794db90cb9c5da624c5d31993176764be4ec05
junpei fujita <lookeron.ss@gmail.com>
2023-06-08 20:01:13 +0900

    feat: support output IAM role name (#3)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "eventbridge_scheduler_arn" {
   description = "ARN of the schedule"
   value       = aws_scheduler_schedule.this.arn
 }
+
+output "iam_role_name" {
+    description = "Name of IAM role"
+    value       = var.schedule_task_role_policy_json_filename == null ? data.aws_iam_role.this[0].name : aws_iam_role.this[0].name
+}
